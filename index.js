4c705f6944858ed06214679931311b20e368a3c8
Sitansu Mandal <85841972+sitansu04@users.noreply.github.com>
2023-01-16 15:51:32 +0530

    updated

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const { connection } = require("./config/db.js");
 const { userRouter } = require("./routes/users.routes.js");
 const {authentication}=require("./middleware/authorization.middleware.js");
 const { postRouter } = require("./routes/posts.routes.js");
+const cors=require("cors")
 require("dotenv").config();
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use("/users", userRouter);
 app.use(authentication)
